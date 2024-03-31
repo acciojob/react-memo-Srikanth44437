dffae21ae5f5196f30fbaa9785b70d21890913f0
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-03-31 10:26:58 +0000

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,37 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+// App.js
+import React, { useState } from 'react';
+import UseMemo from './UseMemo';
+import ReactMemo from './ReactMemo';
 
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [todos, setTodos] = useState(["Task 1", "Task 2"]);
+
+  const handleAddTodo = () => {
+    setTodos(prevTodos => [...prevTodos, "New todo"]);
+  };
+
+  const handleIncrement = () => {
+    setCounter(prevCounter => prevCounter + 1);
+  };
+
+  const handleCustomTask = (e) => {
+    const task = e.target.value;
+    if (task.length > 5) {
+      setTodos(prevTodos => [...prevTodos, task]);
+    }
+  };
+
+  return (
+    <div>
+        <h1>My todos</h1>
+      <button id="add-todo-btn" onClick={handleAddTodo}>Add todo</button>
+      <button id='incr-cnt' onClick={handleIncrement}>Increment</button>
+      <input id='skill-input' type="text" placeholder="Enter task" onBlur={handleCustomTask} />
+      <UseMemo todos={todos} />
+      <ReactMemo counter={counter} />
+    </div>
+  );
+};
+
+export default App;
